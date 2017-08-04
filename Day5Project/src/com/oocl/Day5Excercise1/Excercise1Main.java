8a992adbb11cb0b70ffc878f66652d5f0b40d2fc
ALLARRA <ALLARRA@ZHA-ITA055-W7.corp.oocl.com>
2017-08-04 11:41:27 +0800

    Unfinished

--- Day5Project/src/com/oocl/Day5Excercise1/Excercise1Main.java
+++ Day5Project/src/com/oocl/Day5Excercise1/Excercise1Main.java
@@ -6,6 +6,7 @@ public class Excercise1Main {
 		// TODO Auto-generated method stub
 		System.out.println("Wazzza");
 		System.out.println("Waaaaaaaaazzzzzzza");
+		System.out.print("LOLOLOLOL");
 	}
 
 }
