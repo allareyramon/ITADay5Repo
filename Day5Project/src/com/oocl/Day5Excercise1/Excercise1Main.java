85039abfef4d9251a7930e1b243035d45a4dbeea
ALLARRA <ALLARRA@ZHA-ITA055-W7.corp.oocl.com>
2017-08-04 11:27:14 +0800

    First Commit

---
+++ Day5Project/src/com/oocl/Day5Excercise1/Excercise1Main.java
@@ -0,0 +1,10 @@
+package com.oocl.Day5Excercise1;
+
+public class Excercise1Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wazzza");
+	}
+
+}
