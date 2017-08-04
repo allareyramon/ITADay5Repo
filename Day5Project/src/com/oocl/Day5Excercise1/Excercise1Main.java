99ea88bad9db61fb8cbbd963b483629b0787e743
ALLARRA <ALLARRA@ZHA-ITA055-W7.corp.oocl.com>
2017-08-04 11:34:02 +0800

    SecondChange in Master

--- Day5Project/src/com/oocl/Day5Excercise1/Excercise1Main.java
+++ Day5Project/src/com/oocl/Day5Excercise1/Excercise1Main.java
@@ -5,6 +5,7 @@ public class Excercise1Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Wazzza");
+		System.out.println("Waaaaaaaaazzzzzzza");
 	}
 
 }
